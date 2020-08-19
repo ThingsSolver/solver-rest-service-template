08b7f3089d98bce9aa244ff132756cb492dd0315
darkomarjanovic <darko@thingsolver.com>
2020-08-19 11:43:11 +0200

    update

--- API/{{cookiecutter.microservice_name}}/app/utils/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-import logging.config
-
-
-class AppLogger:
-    def __init__(self, name):
-        logging.config.fileConfig('app/logger.conf')
-        self.logger = logging.getLogger(name)
-
--- API/{{cookiecutter.microservice_name}}/run_server.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo Starting Gunicorn.
-
-exec gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8080 --workers 10
