32442b5f879d2f3a1db5ad4ef3a7349e974ac627
milovanovicm <milos@thingsolver.com>
2020-11-30 13:40:06 +0100

    updated logging conf; updated uvicorn workers;

--- {{cookiecutter.microservice_name}}/run_server.sh
+++ {{cookiecutter.microservice_name}}/run_server.sh
@@ -2,4 +2,4 @@
 
 echo Starting Gunicorn.
 
-exec gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8080 --workers 10
+exec gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8080 --workers 1
